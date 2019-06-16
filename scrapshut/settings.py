eb2e6281c3e95802eadd21352ca3073e6a6da2f2
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2019-06-16 23:27:19 +0530

    ehy

--- scrapshut/settings.py
+++ scrapshut/settings.py
@@ -45,10 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
