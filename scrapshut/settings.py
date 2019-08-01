0cca97fad55d21afe59985c6ad65925ee066c5f3
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2019-08-01 18:00:21 +0530

    added images

--- scrapshut/settings.py
+++ scrapshut/settings.py
@@ -202,7 +202,8 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
